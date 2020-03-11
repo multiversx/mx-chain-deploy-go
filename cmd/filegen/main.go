0ef3e46ab9aa9e3b30b8b7d5def63a9ac2aa2992
Iuga Mihai <mihai9625@gmail.com>
2020-03-11 14:50:03 +0200

    use mcl crypto library

--- cmd/filegen/main.go
+++ cmd/filegen/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ElrondNetwork/elrond-go/crypto"
 	"github.com/ElrondNetwork/elrond-go/crypto/signing"
 	"github.com/ElrondNetwork/elrond-go/crypto/signing/kyber"
+	"github.com/ElrondNetwork/elrond-go/crypto/signing/mcl"
 	"github.com/ElrondNetwork/elrond-go/data/state"
 	"github.com/ElrondNetwork/elrond-go/sharding"
 	"github.com/urfave/cli"
@@ -561,9 +562,7 @@ func getNodesKeyGen(consensusType string) crypto.KeyGenerator {
 
 	switch consensusType {
 	case "bls":
-		suite = kyber.NewSuitePairingBn256()
-	case "bn":
-		suite = kyber.NewBlakeSHA256Ed25519()
+		suite = mcl.NewSuiteBLS12()
 	default:
 		suite = nil
 	}
