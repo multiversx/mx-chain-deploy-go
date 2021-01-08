2ecdf52c50699caeaa54298762cbc78fd96ad0db
Iulian Pascalau <iulian.pascalau@gmail.com>
2021-01-08 13:36:22 +0200

    - default to direct stake

--- cmd/filegen/main.go
+++ cmd/filegen/main.go
@@ -134,7 +134,7 @@ VERSION:
 		Name: "stake-type",
 		Usage: "defines the 2 possible ways to stake the nodes: 'direct' as in direct staking " +
 			"and 'delegated' that will stake the nodes through delegation",
-		Value: "mixed",
+		Value: "direct",
 	}
 	delegationOwnerPublicKey = cli.StringFlag{
 		Name:  "delegation-owner-pk",
